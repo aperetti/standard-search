1ae09d0934ab76b1c3c1780aeac9238ab9d09b1d
Adam Peretti <aperetti3@gatech.edu>
2016-05-29 09:13:47 -0700

    Updated Form Validation

--- src/components/CreateStandard.vue
+++ src/components/CreateStandard.vue
@@ -1,7 +1,7 @@
 <template>
     <form class="form-horizontal" action="{{submitUrl}}" method="POST" enctype="multipart/form-data" v-on:submit.prevent="onSubmit">
       <div class="form-group">
-        <label for="inputStandard" class="col-sm-2 control-label">Standards</label>
+        <label for="inputStandard" class="col-sm-2 control-label">Standard Code</label>
         <div class="col-sm-2">
           <input type="text" class="form-control" placeholder="Standard Code" v-model="code"></input>
         </div>
@@ -12,7 +12,7 @@
           <textarea rows="2" class="form-control" v-model="desc"></textarea>
         </div>
       </div>
-      <div class="form-group" v-if="menu.length">
+      <div class="form-group" v-show="menu.length">
         <label for="inputStandard" class="col-sm-2 control-label">Groups</label>
         <div v-for="group in menu" track-by="$index">
           <div class="col-sm-2">
@@ -37,16 +37,27 @@
       </div>
       <div class="form-group">
         <label class="btn btn-default btn-file">
-          Upload PDF<input id="pdfFile" type="file" name="pdf" style="display: none;">
+          Upload PDF<input id="pdfFile" type="file" name="pdf" style="display: none;" v-model="file">
         </label>
-        <input class="btn btn-primary" type="submit" value="Submit" >
+        <input class="btn btn-primary" type="submit" value="Submit">
+      </div>
+      <div class="col-sm-6 col-sm-offset-3">
+        <div class="panel panel-danger" v-if="!$val.valid">
+          <div class="panel-heading">Errors</div>
+          <ul class="list-group">
+            <li class="list-group-item" v-if="!$val.code.required">Standard Code Required</li>
+            <li class="list-group-item" v-if="!$val.desc.required">Standard Description Required</li>
+            <li class="list-group-item" v-if="!$val.menu.required">(1) Group Required</li>
+            <li class="list-group-item" v-if="!$val.menu.eachLength(1)">Each Group Must Have a Name</li>
+            <li class="list-group-item" v-if="!$val.file.required">File Upload Required</li>
+          </ul>
+        </div>
       </div>
     </form>
 </template>
 
 <script>
   import {apiAddStandard, withToken} from '../api/config'
-  
   export default {
     ready: function () {
     },
@@ -54,6 +65,7 @@
       return {
         code: '',
         desc: '',
+        file: '',
         addGroup: false,
         newGroup: '',
         menu: []
@@ -78,7 +90,6 @@
         this.menu.splice(index)
       },
       onSubmit: function (e) {
-        console.log(this)
         var formData = new window.FormData()
         formData.append('path', this.menu.join('|'))
         formData.append('code', this.code)
@@ -102,6 +113,39 @@
     computed: {
       submitUrl: function () {
         return withToken(apiAddStandard)
+      },
+      $val: function () {
+        let $ = {}
+        $.code = {required: this.code.length > 0}
+        $.desc = {required: this.desc.length > 0}
+        $.menu = {
+          required: this.menu.length > 0,
+          eachLength: (min) => {
+            for (var el in this.menu) {
+              if (this.menu[el].length < min) {
+                return false
+              } else {
+                return true
+              }
+            }
+          }
+        }
+        $.file = {required: this.file.length > 0, type: (extension) => {
+          if (this.file.indexof(extension)) {
+            return true
+          } else {
+            return true
+          }
+        }}
+        $.valid = (function () {
+          for (var type in $) {
+            for (var key in $[type]) {
+              if (!$[type][key]) return false
+            }
+          }
+          return true
+        })()
+        return $
       }
     }
   }
