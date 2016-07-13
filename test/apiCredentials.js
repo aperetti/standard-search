16eaabff5d5e6a0db0c59e9c22d2b22ed657fbcb
aperetti <adam.peretti@gmail.com>
2016-07-12 17:54:58 -0700

    initial apiCredential file. Added to .gitignore.

---
+++ test/apiCredentials.js
@@ -0,0 +1,5 @@
+/* WARNING DO NOT SHARE THIS FILE. IT WILL CONTAIN ACCOUNT ACCESS TO YOUR API */
+/* THIS IS STRICTLY FOR TESTING */
+var apiCredentials = {username: 'userName', password: 'password'}
+export default apiCredentials
+
