f169b4c4f0b1b6ad2e6a6dd8e139e3bbfa24642d
aperetti <adam.peretti@gmail.com>
2016-07-12 17:56:26 -0700

    moved apiCredentials

--- test/apiCredentials.js
+++ test/apiCredentials.js
@@ -1,5 +1,6 @@
 /* WARNING DO NOT SHARE THIS FILE. IT WILL CONTAIN ACCOUNT ACCESS TO YOUR API */
 /* THIS IS STRICTLY FOR TESTING */
+
 var apiCredentials = {username: 'userName', password: 'password'}
 export default apiCredentials
 
