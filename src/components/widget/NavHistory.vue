40fe748618752ee7ace60b89d753210e8cf6c800
aperetti <adam.peretti@gmail.com>
2016-11-07 10:50:43 -0800

    Fixed Link

--- src/components/widget/NavHistory.vue
+++ src/components/widget/NavHistory.vue
@@ -2,7 +2,7 @@
         <drop-down v-show="history.length > 0">
           <template slot='title'>History</template>
           <template slot='dropdown'>
-            <li id='drop {{standard.code}}' v-for='standard in history'><a v-link="{ name: 'standard', params: { standardId: standard.code }}" >{{standard.code}}</a></li>
+            <li id='drop {{standard.code}}' v-for='standard in history'><a v-link="{ name: 'standard', params: { standardId: standard.code }}">{{standard.code}}</a></li>
           </template>
         </drop-down>
 </template>
