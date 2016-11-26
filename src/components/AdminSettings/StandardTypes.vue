400e1e046399adc7ad2ec0d2eab669322d674e32
aperetti <adam.peretti@gmail.com>
2016-11-26 14:31:08 -0800

    Create Admin ability to set StandardTypes

--- src/components/AdminSettings/StandardTypes.vue
+++ src/components/AdminSettings/StandardTypes.vue
@@ -13,40 +13,72 @@
         <tbody>      
       <template v-for='(category, index) in categories'>
         <tr v-if="category.edit">
-          <td><input v-model='category.name'></input></td>
-          <td><input v-model='category.description'></input></td>
-          <td><input v-model='category.regex'></input></td>
-          <td><button @click="confirmCategory(category)">Confirm</button></td>
+          <td><input class="form-control" v-model='category.name'></input></td>
+          <td><input class="form-control" v-model='category.description'></input></td>
+          <td><input class="form-control" v-model='category.regex'></input></td>
+          <td><button class="btn btn-default" @click="confirmCategory(category)">Confirm</button></td>
         </tr>
         <tr v-else>
           <td>{{category.name}}</td>
           <td>{{category.description}}</td>
           <td>{{category.regex}}</td>
-          <td><span @click="editCategory(category)" class="glyphicon glyphicon-edit" style="margin-right: 5px;" /><span @click="deleteCategory(index)" class="glyphicon glyphicon-trash" /></td>
+          <td><span @click="category.edit = true" class="glyphicon glyphicon-edit" style="margin-left: 5px; margin-right: 5px;" /><span @click="handleDelete(index)" class="glyphicon glyphicon-trash" /></td>
         </tr>
       </template>
       <tr>
-        <td><input placeholder="Name" v-model='newCategory.name'></input></td>
-        <td><input placeholder="Description" v-model='newCategory.description'></input></td>
-        <td><input placeholder="Regex" v-model='newCategory.regex'></input></td>
-        <td><button @click='addCategory'>Add <span class="glyphicon glyphicon-plus"/><br/></td>
+        <td><input class="form-control" placeholder="Name" v-model='newCategory.name'></input></td>
+        <td><input class="form-control" placeholder="Description" v-model='newCategory.description'></input></td>
+        <td><input class="form-control" placeholder="Regex" v-model='newCategory.regex'></input></td>
+        <td><button class="btn btn-default" @click='addCategory'>Add <span class="glyphicon glyphicon-plus"/><br/></td>
       </tr>  
       </tbody>
     </table>
+    <div class="row">
+      <div class="col-md-12">
+        <button class="btn btn-default" @click="modals.save = true">Save Changes</button>
+        <button class="btn btn-default" @click="refreshCategories">Reset</button>
+      </div>
+    </div>
+    <div class="row" style="margin-top: 10px;">
+      <div class="col-md-12">
+        <div v-if="edited" class="alert alert-info" role="alert"><b>Heads Up!</b> Changes have not been saved!</div>
+      </div>
+    </div>
+    <base-modal v-on:submit="deleteCategory" v-on:close="modals.delete = false" :dim="modals.delete">
+      <template slot="title">Delete Category</template>
+      <template slot="body">Are you sure you want to delete the following category?</template>
+      <template slot="button">Delete</template>
+    </base-modal>
+    <base-modal v-on:submit="saveReferences" v-on:close="modals.save = false" :dim="modals.save">
+      <template slot="title">Save Changes</template>
+      <template slot="body">Are you sure you want to save the following changes?</template>
+      <template slot="button">Save</template>
+    </base-modal>
   </div>
 </template>
 
 <script>
   import {getCategories, saveCategories} from 'src/api/admin'
+  import BaseModal from 'components/modals/BaseModal'
   export default {
+    components: {
+      BaseModal
+    },
     data () {
       return {
+        edited: false,
+        unwatch: () => false,
         categories: [],
         newCategory: {
           name: '',
           description: '',
           regex: '',
           edit: false
+        },
+        deleteIndex: 0,
+        modals: {
+          delete: false,
+          save: false
         }
       }
     },
@@ -58,10 +90,16 @@
         category.edit = false
       },
       editCategory (category) {
+        console.log('tes')
         category.edit = true
       },
-      deleteCategory (index) {
-        this.categories.splice(index, 1)
+      handleDelete (index) {
+        this.deleteIndex = index
+        this.modals.delete = true
+      },
+      deleteCategory () {
+        this.categories.splice(this.deleteIndex, 1)
+        this.modals.delete = false
       },
       addCategory () {
         this.categories.push(Object.assign({}, this.newCategory))
@@ -70,16 +108,30 @@
         this.newCategory.regex = ''
       },
       refreshCategories () {
+        this.unwatch()
+        this.edited = false
         getCategories().then(res => {
-          this.categories = res.data || []
+          var categories = res.data || []
+          categories.map(cat => {
+            cat.edit = false
+            return cat
+          })
+          this.categories = categories
+          this.unwatch = this.$watch('categories', () => {
+            this.edited = true
+          }, {deep: true})
         })
       },
       saveReferences () {
         saveCategories(this.categories)
         .then(res => {
-          this.$store.dispatch('createAlert', {msg: res.data, type: 'success'})
+          this.edited = false
+          this.modals.save = false
+          this.$store.dispatch('createAlert', {message: res.data, type: 'success'})
         }).catch(e => {
-          this.$store.dispatch('createAlert', {msg: e.data, type: 'danger'})
+          console.log(e)
+          this.modals.save = false
+          this.$store.dispatch('createAlert', {message: e.data, type: 'danger'})
         })
       }
     }
