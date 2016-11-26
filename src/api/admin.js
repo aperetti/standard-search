885d5cd4816b7ce529e3a284d2e1a4fd32b11bb9
aperetti <adam.peretti@gmail.com>
2016-11-26 14:30:53 -0800

    Add API to process an uploaded standard

--- src/api/admin.js
+++ src/api/admin.js
@@ -1,12 +1,14 @@
 import {apiAdminBase, Vue} from 'src/api/config'
 
-var apiSaveCategories = apiAdminBase + 'save_categories'
 // Expects an array with objects of followign schema {}
+var apiSaveCategories = apiAdminBase + 'save_categories'
 export const saveCategories = (refs) => {
-  return Vue.http({url: apiSaveCategories, method: 'POST', data: {references: refs}})
+  return Vue.http({url: apiSaveCategories, method: 'POST', data: {types: refs}})
 }
 
 var apiGetCategories = apiAdminBase + 'categories'
 export const getCategories = () => {
   return Vue.http({url: apiGetCategories, methods: 'GET'})
 }
+
+export const apiProcessPdf = apiAdminBase + 'process_pdf'
