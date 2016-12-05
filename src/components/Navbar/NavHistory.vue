63dea9e9d9060dbd109e61b9fb289ca3b854efed
aperetti <adam.peretti@gmail.com>
2016-12-05 09:56:16 -0800

    Fixed history response to route change.

--- src/components/Navbar/NavHistory.vue
+++ src/components/Navbar/NavHistory.vue
@@ -2,7 +2,7 @@
   <drop-down v-show="history.length > 0">
     <template slot='title'>History</template>
     <template slot='dropdown'>
-      <li :id="'drop ' + standard.code" v-for='standard in history'><router-link :to="{name: 'standard', params: {standardId: standard.code}}">{{standard.code}}</router-link></li>
+      <li :id="'drop ' + standard.code" v-for='standard in history'><router-link :to="{name: 'standard', params: {standardId: standard.id}}">{{standard.code}}</router-link></li>
     </template>
   </drop-down>
 </template>
@@ -21,10 +21,11 @@ export default {
     }).catch((e) => {
       this.history = []
     })
-    bus.on('page-reset', function (arg) {
+
+    bus.on('page-reset', (arg) => {
       if (arg === 'beforeRoute') {
         getHistory().then((response) => {
-          this.history = response.data.history
+          this.history = response.data
         }).catch((e) => {
           this.history = []
         })
