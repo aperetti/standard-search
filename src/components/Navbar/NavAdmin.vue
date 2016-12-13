ed6fd9b91d5f0142528c889e265305c77f8010b7
aperetti <adam.peretti@gmail.com>
2016-12-12 16:53:47 -0800

    removed hide result if current link.

--- src/components/Navbar/NavAdmin.vue
+++ src/components/Navbar/NavAdmin.vue
@@ -3,7 +3,7 @@
     <template slot='title'>Admin</template>
     <template slot='dropdown'>
       <li><router-link :to="{ name: 'createStandard' }" class='cursor'><span class="glyphicon glyphicon-plus"></span>Add New Standard</router-link></li>
-      <li v-if="$route.params.standardId"><router-link :to="{ name: 'editStandard', params: { standardId: $route.params.standardId }}" class='cursor'><span class="glyphicon glyphicon-pencil"></span> Edit Standard</router-link></li>
+      <li v-if="$route.name === 'standard'"><router-link :to="{ name: 'editStandard', params: { standardId: $route.params.standardId }}" class='cursor'><span class="glyphicon glyphicon-pencil"></span> Edit Standard</router-link></li>
       <li><router-link :to="{ name: 'adminSettings' }" class='cursor'><span class="glyphicon glyphicon-wrench"></span>Settings</router-link></li>
     </template>
   </drop-down>
