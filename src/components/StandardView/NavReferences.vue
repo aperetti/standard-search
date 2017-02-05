1253d95be76262f55afa36e346429c2f2d3e1e8d
aperetti <adam.peretti@gmail.com>
2017-02-05 00:18:48 -0800

    Added references to standard views

---
+++ src/components/StandardView/NavReferences.vue
@@ -0,0 +1,34 @@
+<template>
+  <drop-down>
+    <template slot='title'>References</template>
+    <template slot='dropdown'>
+      <li v-for="reference in sortedRefs"><router-link :to="{ name: 'standard', params: { standardId: reference.id }, replace: false}" >{{reference.code}}</a></li>
+   </template>
+  </drop-down>
+</template>
+
+<script>
+  import DropDown from 'components/widget/DropDown'
+  import naturalSort from 'javascript-natural-sort'
+  export default {
+    props: ['references'],
+    components: {
+      DropDown
+    },
+    computed: {
+      sortedRefs: function () {
+        if (this.references && this.references.length > 0) {
+          return this.references.sort((el1, el2) => naturalSort(el1.code, el2.code))
+        }
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  li a {
+   outline: none !important;
+  }
+
+</style>
