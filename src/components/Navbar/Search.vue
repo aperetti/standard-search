037b681b5c63d204c2be534f24398b11c9b20350
aperetti <adam.peretti@gmail.com>
2017-02-05 00:18:29 -0800

    Added animation to search

--- src/components/Navbar/Search.vue
+++ src/components/Navbar/Search.vue
@@ -1,9 +1,14 @@
 <template>
   <span>
     <div class="input-group" style="padding: 8px;">
-      <span @click="searchMethod += 1" class="input-group-addon link" id="sizing-addon1"><span :class="[getSearchGlyph]"></span></span>
+      <span @click="searchMethod += 1" class="input-group-addon link" id="sizing-addon1">
+        <div style="overflow: hidden;">
+          <transition name="slide" mode="out-in">
+            <span v-bind:key="getSearchGlyph" :class="[getSearchGlyph]"></span>
+          </transition>
+        </div>
+      </span>
        <input autocomplete="off" :placeholder='getSearchPlaceholder' class='form-control' id='search' v-model="searchInput" @keyup="getResults"  @focus="focusSearch" @blur="blurResults" />
-
         <div class='list-group float' id='results' v-show='showResults'>
         <a v-if='searchResults && searchResults.length === 0 && searchInput.length > 0 && !loading' id='results-0' @blur="blurResults" class='list-group-item text-left'>No Standards Found</a>
         <a v-if='loading' class='list-group-item text-left'>Loading...</a>
@@ -41,7 +46,8 @@
         searchDesc: true,
         searchPlaceholder: ['Search for standard by code and description', 'Search for standard by code'],
         searchGlyph: ['glyphicon glyphicon-search', 'glyphicon glyphicon-signal'],
-        searchMethod: 1
+        searchMethod: 1,
+        toggleButton: true
       }
     },
     computed: {
@@ -97,7 +103,16 @@
   }
 </script>
 
-<style scoped>
+<style scoped>  
+  .slide-enter-active, .slide-leave-active {
+    transition: all .3s ease;
+  }
+  .slide-enter {
+    top: 20px;
+  }
+  .slide-leave-to {
+    top: -20px;
+  }
   .link {
     cursor: pointer;
   }
